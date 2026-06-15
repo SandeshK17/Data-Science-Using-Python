3681784d2daf8e49f1f47f4a1eeb9c78dec874a9
Sandesh Kashyap <155136430+SandeshK17@users.noreply.github.com>
2026-06-15 16:53:21 +0530

    Initial Commit

--- Data Handling using Pandas/BooleanIndexing.py
+++ Data Handling using Pandas/BooleanIndexing.py
@@ -1,4 +1,4 @@
-# Boolean Indexing in Data Frame
+# Boolean Indexing in the Data Frame
 import pandas as pd
 dic = {'Name':['sachin','Kohli','Hardik'],'Age':[50,34,29]}
 df = pd.DataFrame(dic,index=[True,False,True])
