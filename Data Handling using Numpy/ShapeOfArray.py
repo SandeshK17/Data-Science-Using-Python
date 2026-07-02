9f7e36cdc17f52b62c0765277a2e00096fb4f773
Sandesh Kashyap <155136430+SandeshK17@users.noreply.github.com>
2026-07-02 13:59:10 +0530

    Fix comment formatting and reshape array

--- Data Handling using Numpy/ShapeOfArray.py
+++ Data Handling using Numpy/ShapeOfArray.py
@@ -2,7 +2,7 @@ import numpy as np
 Arr = np.array([[1,2,3,4],[4,5,6,7],[7,8,9,10]])
 print("Order of Array :",Arr.shape)
 
-#We can reshape the array
+# We can reshape the array
 # reshape(row, col)
 Arr = Arr.reshape(12,1)
 print(Arr)
