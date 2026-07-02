088b47a1a38edb9560fe0515248ffa6d751329f5
Sandesh Kashyap <155136430+SandeshK17@users.noreply.github.com>
2026-07-02 14:00:07 +0530

    Refactor variable names for consistency

--- Data Handling using Numpy/ShapeOfArray.py
+++ Data Handling using Numpy/ShapeOfArray.py
@@ -1,8 +1,8 @@
 import numpy as np
-Arr = np.array([[1,2,3,4],[4,5,6,7],[7,8,9,10]])
-print("Order of Array :",Arr.shape)
+arr = np.array([[1,2,3,4],[4,5,6,7],[7,8,9,10]])
+print("Order of Array :",arr.shape)
 
 # We can reshape the array
 # reshape(row, col)
-Arr = Arr.reshape(12,1)
-print(Arr)
+arr = arr.reshape(12,1)
+print(arr)
