ead013db1d2a9c8339f196b80cfa9b8d2eb850e8
Sandesh Kashyap <155136430+SandeshK17@users.noreply.github.com>
2026-07-04 14:01:04 +0530

    Initial Commit

--- Data Handling using Numpy/linspace().py
+++ Data Handling using Numpy/linspace().py
@@ -1,5 +1,5 @@
 # b.linspace()
-# Return evenly spaced numbers over a specified interval.
+# It return evenly spaced numbers over a specified interval.
 import numpy as np
 x = np.linspace(1,20,39)
 print(x)
