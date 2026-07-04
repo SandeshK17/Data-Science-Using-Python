0dd31cfe611ae3ec07432f7719e500ff5b9d0f6d
Sandesh Kashyap <155136430+SandeshK17@users.noreply.github.com>
2026-07-04 13:57:18 +0530

    Initial Commit

--- Data Handling using Numpy/array_1.py
+++ Data Handling using Numpy/array_1.py
@@ -1,9 +1,9 @@
 import numpy as np
-arr = np.array([[1,2,3],[6,7,8]])
-arr2 = np.array([1,2,3,4,5])
-for x in arr:
+arr1 = np.array([[1,2,3],[6,7,8]])         # First Array
+arr2 = np.array([1,2,3,4,5])               # Second Array
+for x in arr1:
     print(x)
-print("Dimension of array 1:",arr.ndim)
+print("Dimension of array 1:",arr1.ndim)
 print("Dimension of array 2:",arr2.ndim)
 
 
