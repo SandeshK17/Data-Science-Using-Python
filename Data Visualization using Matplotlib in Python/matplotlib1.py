d6a7c64f4111553972c6939bd3eb7e2f2397618e
Sandesh Kashyap <155136430+SandeshK17@users.noreply.github.com>
2026-07-05 21:51:43 +0530

    Update File

--- Data Visualization using Matplotlib in Python/matplotlib1.py
+++ Data Visualization using Matplotlib in Python/matplotlib1.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as plt
 
-#initialize data
+# initialize data
 x = [1,2,3,4,5,6]
 y = [56,78,54,66,56,70]
 
-#Plotting the data
+# Plotting the data
 plt.bar(x,y)
 
-#Adding Title
+# Adding Title
 plt.title("Performance Report")
 
 # Adding the labels name
