8c553a57b55b6bf69d99e3ed145855d89d196fc2
Sandesh Kashyap <155136430+SandeshK17@users.noreply.github.com>
2026-07-05 21:50:15 +0530

    Update BooleanIndexing.py
    Updated print statements to include comments for clarity.

--- Data Handling using Pandas/BooleanIndexing.py
+++ Data Handling using Pandas/BooleanIndexing.py
@@ -3,5 +3,5 @@ import pandas as pd
 dic = {'Name':['sachin','Kohli','Hardik'],'Age':[50,34,29]}
 df = pd.DataFrame(dic,index=[True,False,True])
 print(df)
-print(df.loc[True])
-print(df.loc[False])
+print(df.loc[True]) # Print only true index
+print(df.loc[False]) # Print only false index
