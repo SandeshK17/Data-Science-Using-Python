a86c5540511321047d6bf67c147efe84b87e17ba
Sandesh Kashyap <155136430+SandeshK17@users.noreply.github.com>
2026-07-07 23:51:40 +0530

    Update File

--- Data Handling using Pandas/Dataframe.py
+++ Data Handling using Pandas/Dataframe.py
@@ -1,6 +1,6 @@
 import pandas as pd
 df = pd.DataFrame()
-name = pd.Series(['Rohit','Virat','Dhoni'])
+name = pd.Series(['Rohit Sharma','Virat Kohli','MS Dhoni'])
 team = pd.Series(['MI','RCB','CSK'])
 dic = {'Name':name,'Team':team}
 df = pd.DataFrame(dic)
