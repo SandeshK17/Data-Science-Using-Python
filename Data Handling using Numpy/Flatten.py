7352e4a1eba44e11a319789bf04943785da69405
Sandesh Kashyap <155136430+SandeshK17@users.noreply.github.com>
2026-07-08 23:45:18 +0530

    Update File

--- Data Handling using Numpy/Flatten.py
+++ Data Handling using Numpy/Flatten.py
@@ -1,4 +1,4 @@
-# Flatten is a simple method to transform a matrix into a one-dimensional numpy array.
+#Flatten is a simple method to transform a matrix into a one-dimensional numpy array.
 import numpy as np
 a = np.array([[1,2,3],[4,5,6],[7,8,9]])
 print("matrix a is :\n",a)
