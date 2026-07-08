39f1c425d95c2f30baf09dffb28af6e470b980e4
Sandesh Kashyap <155136430+SandeshK17@users.noreply.github.com>
2026-07-08 23:45:49 +0530

    Update File

--- Data Handling using Numpy/ShapeOfArray.py
+++ Data Handling using Numpy/ShapeOfArray.py
@@ -2,7 +2,7 @@ import numpy as np
 arr = np.array([[1,2,3,4],[4,5,6,7],[7,8,9,10]])
 print("Order of Array :",arr.shape)
 
-# We can reshape the array
+#We can reshape the array
 # reshape(row, col)
 arr = arr.reshape(12,1)
 print(arr)
