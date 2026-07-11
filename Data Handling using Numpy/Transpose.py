839d14dbb3efb23fdafd77c0c7e2f2352a29d3f9
Sandesh Kashyap <155136430+SandeshK17@users.noreply.github.com>
2026-07-11 22:40:12 +0530

    Update File
    Updated comment to clarify the purpose of the methods.

--- Data Handling using Numpy/Transpose.py
+++ Data Handling using Numpy/Transpose.py
@@ -4,6 +4,6 @@ a = np.array([[1,2,3],[4,5,6],[7,8,9]])
 print("The given Matrix is :\n",a)
 print("Traspose of the given Matrix is :\n",a.transpose())
 
-# These are other methods for finding Transpose.
+# These are other methods for finding Transpose of a matrix.
 # print("Traspose of the given Matrix is :",np.transpose(a))
 # print("Traspose of the given Matrix is :",a.T)
